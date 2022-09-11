8f3af72e42d20082398e50f2798d1a7c899cfd16
Rafael Melchioretto <melchiorettorafael@gmail.com>
2022-09-11 16:56:35 -0300

    Inclusao Metodo Calcula Nota

--- testeInteiros/src/testeInteiros/app/Main.java
+++ testeInteiros/src/testeInteiros/app/Main.java
@@ -1,17 +1,23 @@
 package testeInteiros.app;
 
+import testeInteiros.entities.Aluno;
+
 public class Main {
 	/*Main é o método auto-executável do Java*/
 	public static void main(String[] args) {
-			
-		double nota1 = 90;
-		double nota2 = 70;
-		double nota3 = 50;
-		double nota4 = 88;
+		Aluno aluno1 = new Aluno();
 		
-		double mediaFinal = (nota1+nota2+nota3+nota4)/4;
+		aluno1.setNome("Rafael");
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90.7);
 		
-		System.out.println("Media das notas = "+mediaFinal);
+		System.out.println("Media Aluno = "+String.format("%.2f",aluno1.getMedia()));
+		
+		
+		
+	
 	}
 
 }
