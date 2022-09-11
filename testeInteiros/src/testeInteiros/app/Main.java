2fc64583a0608f2762c7f98d07d85465a19fdc31
Rafael Melchioretto <melchiorettorafael@gmail.com>
2022-09-11 12:55:08 -0300

    Add Classe Aluno

---
+++ testeInteiros/src/testeInteiros/app/Main.java
@@ -0,0 +1,17 @@
+package testeInteiros.app;
+
+public class Main {
+	/*Main é o método auto-executável do Java*/
+	public static void main(String[] args) {
+			
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		double mediaFinal = (nota1+nota2+nota3+nota4)/4;
+		
+		System.out.println("Media das notas = "+mediaFinal);
+	}
+
+}
---
+++ testeInteiros/src/testeInteiros/entities/Aluno.java
@@ -0,0 +1,16 @@
+package testeInteiros.entities;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+}
