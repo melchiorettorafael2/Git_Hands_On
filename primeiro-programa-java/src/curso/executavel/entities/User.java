e5961744fd0c5eebd2c6e2d056d86530222dc454
Rafael Melchioretto <melchiorettorafael@gmail.com>
2022-09-11 07:33:27 -0300

    Adicionando Pacote Entidades

---
+++ primeiro-programa-java/src/curso/executavel/entities/User.java
@@ -0,0 +1,59 @@
+package curso.executavel.entities;
+
+import java.util.Objects;
+
+public class User {
+	private String nome;
+	private String email;
+	private String id;
+	
+	public User(String nome, String email, String id) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+	
+	
+}
